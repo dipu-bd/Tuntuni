0c77379ab9aae865e4de092b016fc5c5f3543855
dipu-bd <dipu.sudipta@gmail.com>
2016-06-30 15:58:00 +0600

    Server.java: implemented start and stop functions. Logs.java: for holding log messages. blog: to store development time blogs.

--- src/main/java/org/tuntuni/connection/Server.java
+++ src/main/java/org/tuntuni/connection/Server.java
@@ -15,12 +15,100 @@
  */
 package org.tuntuni.connection;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.tuntuni.util.Logs;
+
 /**
  * To listen and respond to clients sockets.
  */
-public class Server {
+public final class Server {
 
-    public Server() {
+    public static final int PRIMARY_PORT = 24914;
+    public static final int BACKUP_PORT = 42016;
+
+    public static final Logger logger = Logger.getLogger(Server.class.getName());
+
+    private ServerSocket mServer; // holds main server socket
+
+    /**
+     * Create a new Server. It does not start the server automatically. Please
+     * call {@linkplain start()} to start the server.
+     *
+     * @throws IOException
+     */
+    public Server() throws IOException {
 
     }
+
+    /**
+     * Creates a server and start listening.
+     *
+     * @throws IOException Server failed load in both Primary and backup ports.
+     */
+    public void start() throws IOException {
+
+        // first try to create server on primary port
+        try {
+            mServer = new ServerSocket(PRIMARY_PORT);
+        } catch (IOException ex) {
+            logger.log(Level.INFO, Logs.SERVER_PRIMARY_PORT_FAILS);
+
+            // primary port fails. start in backup port
+            try {
+                mServer = new ServerSocket(PRIMARY_PORT);
+            } catch (IOException ix) {
+                logger.log(Level.SEVERE, Logs.SERVER_BACKUP_PORT_FAILS);
+                // backup fails. throw error
+                throw ix;
+            }
+        }
+        // now the server should be created. just listen and wait for clients. 
+        listen();
+    }
+
+    /**
+     * Force stop the server. If succeeded returns true otherwise false.
+     *
+     * @return True if server is stopped. False otherwise.
+     */
+    public boolean stop() {
+        if (mServer == null) {
+            // no server found
+            return true;
+        }
+        try {
+            // check if server is running
+            if (mServer.isBound()) {
+                // close normally
+                mServer.close();
+                // wait until closed
+                while (!mServer.isClosed()) {
+                    logger.log(Level.INFO, Logs.SERVER_IS_CLOSING);
+                    Thread.sleep(50);
+                }
+            }
+            return true;
+        } catch (IOException | InterruptedException ex) {
+            logger.log(Level.SEVERE, Logs.SERVER_FAILED_CLOSING, ex);
+            return false;
+        }
+    }
+
+    /**
+     * Returns true if the server is up and running; false otherwise.
+     */
+    public boolean isActive() {
+        return (mServer != null && mServer.isBound());
+    }
+
+    /**
+     * Start listening for the clients to connect.
+     */
+    private void listen() {
+
+    }
+
 }
---
+++ src/main/java/org/tuntuni/util/Logs.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2016 Sudipto Chandra.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tuntuni.util;
+
+/**
+ * Lists some log messages used through-out the application.
+ */
+public abstract class Logs {
+
+    public static final String INITIALIZE = "Log initialized",
+            
+            SERVER_PRIMARY_PORT_FAILS = "Failed to start server on primary port. Switching to backup port",
+            SERVER_BACKUP_PORT_FAILS = "Server fails to start on backup port. Server is not running.",
+            SERVER_IS_CLOSING = "Waiting until the server is stopped.",
+            SERVER_FAILED_CLOSING = "Failed to close the server",
+            
+            FINALIZE = "Log finalized ";
+
+}
