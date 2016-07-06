0f7afb59ff468950555b0b0634b4cb4cbc63716d
dipu-bd <dipu.sudipta@gmail.com>
2016-07-06 06:01:50 +0600

    Auto height change in message text area

--- src/main/java/org/tuntuni/controllers/MessagingController.java
+++ src/main/java/org/tuntuni/controllers/MessagingController.java
@@ -15,18 +15,21 @@
  */
 package org.tuntuni.controllers;
 
+import com.sun.javafx.tk.Toolkit;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import javafx.application.Platform;
 import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Region;
 import org.tuntuni.Core;
 import org.tuntuni.components.MessageBox;
 import org.tuntuni.connection.Client;
@@ -39,28 +42,34 @@ import org.tuntuni.models.Message;
  * send text button. Above is the conversation history. </p>
  */
 public class MessagingController implements Initializable {
-
+    
     private Client mClient;
-
+    
     @FXML
     private TextArea messageText;
     @FXML
     private ListView messageList;
-
+    @FXML
+    private Label errorLabel;
+    
+    private final double MINIMUM_HEIGHT = 70D;
+    private final double MAXIMUM_HEIGHT = 150D;
+    
     public MessagingController() {
     }
-
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Core.instance().messaging(this);
     }
-
+    
     public void setClient(Client client) {
         mClient = client;
         loadAll();
     }
-
+    
     private void loadAll() {
+        errorLabel.setText("");
         messageText.clear();
         messageList.getItems().clear();
 
@@ -74,38 +83,7 @@ public class MessagingController implements Initializable {
                     });
         }
     }
-
-    @FXML
-    private void handleSendMessage(ActionEvent event) {
-        String text = messageText.getText().trim();
-        if (text.isEmpty()) {
-            showAlert("Message is too short");
-            return;
-        } 
-        sendMessage(text);
-        messageText.clear();
-    }
-
-    private void sendMessage(String text) {
-        if (mClient == null) {
-            showAlert("The receiver is unknown");
-            return;
-        }
-
-        Message message = new Message();
-        message.setText(text);
-        if (!mClient.sendMessage(message)) {
-            showAlert("Could not send message.");
-            Platform.runLater(() -> {
-                messageText.setText(text + "\r\n" + messageText.getText());
-            });
-            return;
-        }
-
-        mClient.addMessage(message);
-        message.setClient(mClient);
-    }
-
+    
     private void showHistory() {
         int elems = messageList.getItems().size();
         for (int i = elems; i < mClient.messageProperty().getSize(); ++i) {
@@ -116,8 +94,68 @@ public class MessagingController implements Initializable {
             messageList.scrollTo(messageList.getItems().size() - 1);
         }
     }
-
-    private void showAlert(String message) {
-        new Alert(Alert.AlertType.INFORMATION, message).showAndWait();
+    
+    @FXML
+    private void handleMessageTyped(KeyEvent evt) {
+        if (evt.getCharacter().length() > 0) {
+            setTextAreaHeight();
+            if (evt.getCharacter().charAt(0) == 13) {
+                if (evt.isShiftDown()) {                    
+                    messageText.appendText("\n");
+                } else {
+                    handleSendMessage(null);        
+                    evt.consume();
+                }
+            }
+        }
+    }
+    
+    @FXML
+    private void handleSendMessage(ActionEvent event) {
+        errorLabel.setText("");
+        String text = messageText.getText().trim();
+        
+        if (text.isEmpty()) {
+            errorLabel.setText("Message is too short");
+            return;
+        }
+        sendMessage(text);
+    }
+    
+    private void sendMessage(String text) {
+        if (mClient == null) {
+            errorLabel.setText("The receiver is unknown");
+            return;
+        }
+        
+        Message message = new Message();
+        message.setText(text);
+        if (!mClient.sendMessage(message)) {
+            errorLabel.setText("Could not send message.");
+            return;
+        }
+        
+        mClient.addMessage(message);
+        message.setClient(mClient);
+        messageText.clear();
+    }
+    
+    private void setTextAreaHeight() {
+        // elements
+        Region content = (Region) messageText.lookup(".content");
+        ScrollBar scrollBarv = (ScrollBar) messageText.lookup(".scroll-bar:vertical");
+        // get the height of content
+        double height = MINIMUM_HEIGHT;
+        if (messageText.getText().length() > 10) {
+            Insets padding = messageText.getPadding();
+            height = content.getHeight() + padding.getTop() + padding.getBottom();
+        }
+        // set height
+        height = Math.max(MINIMUM_HEIGHT, height);
+        height = Math.min(MAXIMUM_HEIGHT, height);
+        messageText.setMinHeight(height);
+        messageText.setPrefHeight(height);
+        // enable or the scroll bar
+        scrollBarv.setVisibleAmount(MAXIMUM_HEIGHT);
     }
 }
