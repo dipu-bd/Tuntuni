ecf9064231f576a8fe1b496c41258403aaf60be7
dipu-bd <dipu.sudipta@gmail.com>
2016-07-11 10:32:56 +0600

    video call designed

--- src/main/java/org/tuntuni/controllers/VideoCallController.java
+++ src/main/java/org/tuntuni/controllers/VideoCallController.java
@@ -17,9 +17,18 @@ package org.tuntuni.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import org.tuntuni.Core;
 import org.tuntuni.connection.Client;
+import org.tuntuni.video.VideoCapturer;
+import org.tuntuni.video.VideoFormat;
+import org.tuntuni.video.VideoRenderer;
 
 /**
  * Controller for video calling. It shows video in background. 
@@ -28,9 +37,25 @@ public class VideoCallController implements Initializable {
  
     private Client mClient; 
     
+    @FXML
+    private Button callButton;
+    @FXML
+    private ImageView userPhoto;
+    @FXML
+    private Label userName;
+    @FXML
+    private Pane viewPane;
+    
+    private VideoFormat mFormat;
+    private VideoCapturer mCapturer;
+    private VideoRenderer mRenderer;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Core.instance().videocall(this);
+        
+        mFormat = new VideoFormat();
+        mCapturer = new VideoCapturer(mFormat);
     }     
  
     public void setClient(Client client) {
@@ -40,5 +65,16 @@ public class VideoCallController implements Initializable {
 
     private void loadAll() {
         
+        if(mClient != null && mClient.getUserData() != null) {
+            userName.setText(mClient.getUserData().getUserName());
+            userPhoto.setImage(mClient.getUserData().getAvatar(
+                    userPhoto.getFitWidth(), userPhoto.getFitHeight()));
+        }
     }
+    
+    @FXML
+    private void startVideoCall(ActionEvent evt) {
+        
+    }
+    
 }
